da3cf55928194ee3ea408668d7f105819a8898d9
Maksim Kostromin <maksim.kostromin@sigma.software>
2017-02-03 21:52:36 +0200

    Initial commit

---
+++ src/main/java/daggerok/vault/Main.java
@@ -0,0 +1,18 @@
+package daggerok.vault;
+
+import daggerok.fallback.FallbackConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootApplication
+@Import(FallbackConfig.class)
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("bootstrapping...");
+        SpringApplication.run(Main.class, args);
+    }
+}
