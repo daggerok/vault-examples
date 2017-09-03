eeb25e44c266d0201bdf41245cd0be74c3b3190a
Maksim Kostromin <daggerok@gmail.com>
2017-09-04 01:22:10 +0300

    Restructure; Repackage; Fix build..

---
+++ 01-skip-this-useless-shit/src/main/java/daggerok/vault/client/Main.java
@@ -0,0 +1,13 @@
+package daggerok.vault.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class Main {
+
+  public static void main(String[] args) {
+    log.info("bootRepackage stub");
+  }
+}
