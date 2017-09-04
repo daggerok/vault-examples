3a381c48d261ff3d81621b13050d6a12606277ea
Maksim Kostromin <daggerok@gmail.com>
2017-09-04 05:55:58 +0300

    Add MySQL Backend (vault mount mysql)

---
+++ 03-vault-mount-mysql/src/main/java/daggerok/App.java
@@ -0,0 +1,13 @@
+package daggerok;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class App {
+
+  public static void main(String[] args) {
+    log.info("bootRepackage stub");
+  }
+}
